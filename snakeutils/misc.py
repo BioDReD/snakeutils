fc0840a8248366a44cc24d6b0d85b769231eb0c7
luka.pavageau <luka.pavageau@etu.univ-nantes.fr>
2025-06-06 09:36:59 +0200

    permet de passer les wildcards pour formatter les params au besoin, et gère les arguments ayant un None dans la config

--- snakeutils/misc.py
+++ snakeutils/misc.py
@@ -1,13 +1,19 @@
 
 
-def params_to_CLI(params:dict, sep=" ", br=" ") :
+def params_to_CLI(params:dict, sep=" ", br=" ", wildcards=None) :
     """
     Loads CLI parameters from a dict and generates a single string to add to the command line.
     A double dash is prepended to multiletters parameters and a single dash is prepended to single letter parmaters.
     """
     final = ""
     for key, value in params.items() :
+        if value is None : 
+            value = ""
+        
         key = f"-{key}" if len(key) == 1 else f"--{key}"
         final += f"{key}{sep}{value}{br}"
     
+    if wildcards is not None : 
+        final = final.format(wildcards=wildcards)
+
     return final
