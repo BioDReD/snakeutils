5993f79d91f5bbcf7fd2876a76a550ebe080ae1c
luka.pavageau <luka.pavageau@etu.univ-nantes.fr>
2026-02-24 10:04:02 +0100

    loads chromosome list from .fai

--- snakeutils/misc.py
+++ snakeutils/misc.py
@@ -1,4 +1,4 @@
-
+from pathlib import Path
 
 def params_to_CLI(params:dict, sep=" ", br=" ", wildcards=None) :
     """
@@ -17,3 +17,16 @@ def params_to_CLI(params:dict, sep=" ", br=" ", wildcards=None) :
         final = final.format(wildcards=wildcards)
 
     return final
+
+def infer_chromosome_list_from_genome(genome_file:"str|Path") : 
+    """
+    Given a genome file (.fa/.fasta), tries to load the index file (.fasta.fai) that is expected next to the genome file.
+    Loads the chromosome names from it and returns them as a list.
+    """
+    index = Path(str(genome_file) + ".fai")
+    if not index.exists() : 
+        raise ValueError(f"The index file {index} could not be found ! This file must be present and can be generated with the command `samtools faidx {genome_file}`")
+    
+    with open(index) as fp : 
+        chroms = [l.split()[0] for l in fp if l]
+        return chroms
